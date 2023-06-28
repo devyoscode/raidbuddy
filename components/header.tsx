cfb4e7c3aeb8090cb7ed86c2ec66949f589b71ed
Devyos <134316908+devyoscode@users.noreply.github.com>
2023-06-28 16:15:11 -0700

    feat: develop comp header

---
+++ components/header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { iComponent } from "@/types/types";
+import Button from "@/components/ui/button";
+
+const Header = ({ children, title, text, button }: iComponent) => {
+  return (
+    <div className="flex items-center justify-between my-5 py-2 border-dashed border-2 border-red-500">
+      <h2 className="w-1/2 text-7xl font-semibold leading-[5rem]">{title}</h2>
+      <div className="w-1/2">
+        <p className="font-light text-lg my-3 text-slate-900/80">{children}</p>
+        {button && <Button variant="secondary">{text}</Button>}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
