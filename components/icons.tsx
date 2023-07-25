ac2b4d2f46d32daddd9998dca477dea617f17640
Devyos <117061812+robced@users.noreply.github.com>
2023-07-25 16:09:18 -0700

    feat: add check icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -213,3 +213,22 @@ export const LogoutIcon = () => {
     </svg>
   );
 };
+
+export const SuccessIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={3}
+      stroke="currentColor"
+      className="w-4 h-4 mx-[39px] my-1"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 12.75l6 6 9-13.5"
+      />
+    </svg>
+  );
+};
