d52be42b9b6f0aa65ebd4b10c2ba2e4bb7edfff8
Devyos <117061812+robced@users.noreply.github.com>
2023-07-25 16:10:56 -0700

    feat: input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -4,8 +4,14 @@ import { iInput } from "@/types/types";
 const Input = ({ title, setState, state }: iInput) => {
   return (
     <div className="">
-      <h5 className="">{title}</h5>
-      <input type="text" className="" />
+      <h5 className="text-sm font-medium text-slate-900 my-1">{title}</h5>
+      <input
+        type="text"
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+        className="h-10 w-full bg-slate-white border-slate-400/50 rounded border-2 shadow-lg px-3 text-slate-900/50"
+        required
+      />
     </div>
   );
 };
