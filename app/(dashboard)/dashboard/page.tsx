13ddab7b4d190f2e897437d8481e34e1b25a279b
Devyos <117061812+robced@users.noreply.github.com>
2023-07-25 16:09:04 -0700

    feat: add teams query

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect } from "react";
 import { useSession } from "next-auth/react";
-import Spinner from "@/components/ui/spinner";
 import { useRouter } from "next/navigation";
 import RecentRaids from "@/components/recentRaids";
 import TeamActivity from "@/components/teamActivity";
@@ -11,15 +10,41 @@ import Button from "@/components/ui/button";
 import { PlusIcon } from "@/components/icons";
 import DashFooter from "@/components/dashFooter";
 import { useStateContext } from "@/context/statecontext";
+import { getMembership } from "@/lib/getMembership";
+import TeamForm from "@/components/teamForm";
+import { Spinner } from "@/components/ui/spinner";
 
 const Page = () => {
   const router = useRouter();
   const session = useSession();
-  const { setRaidCreation, setRoamCreation } = useStateContext();
+  const {
+    setRaidCreation,
+    setUserTeams,
+    userTeams,
+    isTeamMember,
+    setIsTeamMember,
+    setRoamCreation,
+  } = useStateContext();
 
   useEffect(() => {
     if (session.status === "unauthenticated") router.push("/");
   });
+
+  useEffect(() => {
+    if (session.status !== "authenticated") return;
+
+    getMembership(session.data.user.id).then((response) => {
+      if (response.length < 1) {
+        setIsTeamMember("false");
+        return;
+      }
+
+      if (response.length >= 1) {
+        setUserTeams(response);
+        setIsTeamMember("true");
+      }
+    });
+  }, [session]);
 
   if (session.status === "loading") {
     return (
@@ -29,7 +54,8 @@ const Page = () => {
     );
   } else if (session.status === "authenticated") {
     return (
-      <div className="max-w-7xl w-full mx-auto  flex flex-col mt-5">
+      <div className="max-w-7xl w-full mx-auto  flex flex-col mt-5 relative">
+        {isTeamMember === "false" && <TeamForm userId={session.data.user.id} />}
         <div className="w-full h-12 flex items-center justify-between">
           <div className=" h-full flex flex-col">
             <h3 className="text-xl font-semibold">Overview</h3>
