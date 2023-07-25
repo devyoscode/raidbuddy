0e748c2b5c6c4959fd4b56dc137a3a983a8f35c6
Devyos <117061812+robced@users.noreply.github.com>
2023-07-25 16:12:14 -0700

    feat: form popup

--- components/ui/popUpCreationForm.tsx
+++ components/ui/popUpCreationForm.tsx
@@ -6,15 +6,21 @@ const PopUpCreationForm = ({
   setBooleanState,
   title,
   text,
+  mandatory,
 }: iComponent) => {
-  if (!setBooleanState) return;
+  const handleClick = () => {
+    if (!mandatory) {
+      if (!setBooleanState) return;
+      setBooleanState(false);
+    }
+  };
   return (
     <div className="absolute min-h-screen w-full min-w-screen h-full bg-slate-900/10 transition duration-75 backdrop-blur flex items-center justify-center z-20">
       <div
-        onClick={() => setBooleanState(false)}
+        onClick={() => handleClick()}
         className="w-full h-full absolute bg-slate-900/10"
       />
-      <div className="max-w-7xl w-full h-[700px] flex flex-col bg-slate-100 rounded z-50 px-10 py-5">
+      <div className="max-w-7xl w-fit h-fit flex flex-col bg-slate-100 rounded z-50 px-10 py-5">
         <div className="w-full my-5">
           <h3 className="font-semibold text-2xl text-slate-900">{title}</h3>
           <p className="font-light text-slate-900/50">{text}</p>
